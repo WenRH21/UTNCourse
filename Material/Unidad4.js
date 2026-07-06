75763cc89b0c14ecc1fc6699dc5d5983706d4253
Wendy Hirschfeld <wendyhirschfeld9@gmail.com>
2026-07-06 19:04:35 -0300

    Completed Unit 4

---
+++ Material/Unidad4.js
@@ -0,0 +1,16 @@
+// Unidad 4 - Ejercicio 1 
+const producto = "Mousse Gamer";
+const precio = 1200;
+const disponible = true;
+
+console.log(`El ${producto} cuesta ${precio} y su disponibilidad es: ${disponible}.`);
+
+
+// Unidad 4 - Ejercicio 3
+const productos = ["monitor", "teclado", "mouse"];
+
+for (let i = 0; i < productos.length; i++) {
+  console.log(`${productos[i]}`);
+}
+
+
